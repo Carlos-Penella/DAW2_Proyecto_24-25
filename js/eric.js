306d10b5e7bd0b91b9a65605cdd2404fa6973fc6
Eric Córdoba <149142578+EricCordobaDev@users.noreply.github.com>
2025-01-17 12:14:25 +0100

    Add files via upload

--- js/eric.js
+++ js/eric.js
@@ -1,61 +1,58 @@
-
-window.onload = draw
-
-
-let canvas = null
-let ctx = null
-
-let black = 'black'
-let white = 'white'
-let blue = '#61b8ee'
-
-let grey = '#d3d3d3'
-
-function draw() {
-     // recuperamos el lienzo del canvas y su ctxo... 
-     canvas = document.getElementById("canvas")
-     ctx = canvas.getContext("2d")
-     ctx.globalAlpha = 0.5
-
-     w = canvas.clientWidth
-     h = canvas.clientHeight
-     unit = h * 0.05
-
-     canvas.width = w
-     canvas.height = h
-
-     rejilla()
-     techo()
-}
-
 function techo() {
-ctx.strokeStyle = black;
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.moveTo(100, h / 2);
-ctx.lineTo(600, h / 2);
-ctx.stroke();
+     ctx.strokeStyle = black;
+     ctx.lineWidth = 6;
+
+     //poste
+
+     ctx.beginPath();
+     ctx.moveTo(unit*4, unit*1);
+     ctx.lineTo(unit*4, unit*3);
+     ctx.stroke();
+     ctx.closePath();
+
+     //tejado
+
+     ctx.beginPath();
+     ctx.moveTo(unit*4, unit*3);
+     ctx.lineTo(unit*8, unit*10);    
+     ctx.lineTo(unit*-1, unit*10);  
+     ctx.lineTo(unit*4, unit*3);
+     ctx.fillStyle = 'red';
+     ctx.fill();
+     ctx.stroke();
+     ctx.closePath();
+
+     //bandera
+
+     ctx.beginPath();
+     ctx.moveTo(unit*4, unit*1);
+     ctx.lineTo(unit*4, unit*0);
+     ctx.lineTo(unit*8, unit*2);
+     ctx.lineTo(unit*5, unit*1);
+     ctx.lineTo(unit*4, unit*1);
+     ctx.stroke();
+     ctx.closePath();
+
+
+     //cuerpo
+
+     ctx.beginPath();
+     ctx.moveTo(unit*7, unit*10);
+     ctx.lineTo(unit*7, unit*16);
+     ctx.lineTo(unit*0, unit*16);
+     ctx.lineTo(unit*0, unit*10);
+     ctx.stroke();
+     ctx.closePath();
+
+     //ventana
+
+     ctx.beginPath();
+     ctx.moveTo(unit*5, unit*15);
+     ctx.lineTo(unit*5, unit*11);
+     ctx.lineTo(unit*2, unit*11);
+     ctx.lineTo(unit*2, unit*15);
+     ctx.lineTo(unit*5, unit*15);
+     ctx.stroke();
+     ctx.closePath();
+
 }
-
-// REJILLA
-function rejilla() {
-     ctx.strokeStyle = black
-     ctx.lineWidth = 1
-
-     for (var y = 0; y <= h; y = y + unit) {
-          ctx.beginPath()
-          ctx.moveTo(0, y)
-          ctx.lineTo(w, y)
-          ctx.stroke();
-     }
-
-     for (var x = 0; x <= w; x = x + unit) {
-          ctx.strokeStyle = black
-          ctx.beginPath()
-          ctx.moveTo(x, 0)
-          ctx.lineTo(x, h)
-          ctx.stroke();
-     }
-}
-
-
