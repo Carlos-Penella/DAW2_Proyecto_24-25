f12a3462ad7418fd9293d90982b2cb8c7554a775
Eric Córdoba <149142578+EricCordobaDev@users.noreply.github.com>
2025-01-24 12:05:14 +0100

    Add files via upload

--- js/eric.js
+++ js/eric.js
@@ -14,44 +14,25 @@ function techo() {
 
      ctx.beginPath();
      ctx.moveTo(unit * 4, unit * 1);
-     ctx.bezierCurveTo(unit * 4.5, unit * 0.5, unit * 5.5, unit * 1.5, unit * 8, unit * 1);
-     ctx.lineTo(unit * 8, unit * 2.5);
-     ctx.bezierCurveTo(unit * 6, unit * 3, unit * 6, unit * 2, unit * 4, unit * 2.5);
+     ctx.bezierCurveTo(unit * 4.5, unit * 0.3, unit * 5.5, unit * 1.5, unit * 9, unit * 1);
+     ctx.lineTo(unit * 9, unit * 2.5);
+     ctx.bezierCurveTo(unit * 6, unit * 3.5, unit * 6, unit * 2, unit * 4, unit * 2.5);
      ctx.lineTo(unit * 4, unit * 1);
      ctx.stroke();
-     ctx.closePath();
-
-     //ventana
-
-     ctx.beginPath();
-     ctx.moveTo(unit * 6, unit * 15);
-     ctx.lineTo(unit * 6, unit * 11);
-     ctx.lineTo(unit * 2, unit * 11);
-     ctx.lineTo(unit * 2, unit * 15);
-     ctx.lineTo(unit * 6, unit * 15);
-     ctx.stroke();
-     ctx.closePath();
+     ctx.closePath();    
 
      //cuerpo
    
+     ctx.strokeStyle = 'black';
      ctx.beginPath();
      ctx.moveTo(unit * 8, unit * 10);
-     ctx.lineTo(unit * 8, unit * 16);
+     ctx.lineTo(unit * 0, unit * 10);
+     ctx.lineTo(unit * 0, unit * 18);        
+     ctx.moveTo(unit * 8, unit * 10);
+     ctx.lineTo(unit * 8, unit * 18);
      ctx.stroke();    
-     ctx.closePath();
-
-     //Puerta    
-
-     ctx.fillStyle = 'brown';
-     ctx.beginPath();
-     ctx.moveTo(unit * 6, unit * 16);
-     ctx.lineTo(unit * 6, unit * 11);
-     ctx.lineTo(unit * 2, unit * 11);
-     ctx.lineTo(unit * 2, unit * 16);
-     ctx.lineTo(unit * 6, unit * 16);
-     ctx.stroke();
-     ctx.fill();
-     ctx.closePath();     
+     ctx.closePath();      
+    
 
      // ladrillos
 
@@ -59,7 +40,7 @@ function techo() {
      ctx.lineWidth = 1;
 
      fila = 0  // Contador para las filas
-     for (y = unit * 10; y <= unit * 15; y += unit) {
+     for (y = unit * 10; y <= unit * 17; y += unit) {
 
           //Con esto vamos alternando las trazas para simular ladrillos
           let desplazamiento = (fila % 2 == 0) ? 0 : unit
@@ -77,11 +58,27 @@ function techo() {
                ctx.stroke();
           }
           fila++
-     }
+     }      
+
+     //Puerta
+
+     ctx.lineWidth = 4;
+     ctx.strokeStyle = 'brown';
+     ctx.fillStyle = 'brown';
+     ctx.beginPath();
+     ctx.moveTo(unit * 7.5, unit * 17);
+     ctx.lineTo(unit * 7.5, unit * 11);
+     ctx.lineTo(unit * 5, unit * 10.5);
+     ctx.lineTo(unit * 5, unit * 17.5);
+     ctx.lineTo(unit * 7.5, unit * 17);
+     ctx.stroke();
+     ctx.fill();
+     ctx.closePath();
 
      //tejado
 
      ctx.lineWidth = 3;
+     ctx.strokeStyle = 'red';
      ctx.beginPath();
      ctx.moveTo(unit * 4, unit * 3);
      ctx.lineTo(unit * 9, unit * 10);
@@ -91,4 +88,36 @@ function techo() {
      ctx.fill();
      ctx.stroke();
      ctx.closePath();
+
 }
+
+function sol() {
+     ctx.strokeStyle = 'yellow';
+     ctx.fillStyle = 'yellow';
+     ctx.beginPath();
+     ctx.arc(unit * 40, unit * 0, unit * 7, 0, 2 * Math.PI);
+     ctx.stroke();
+     ctx.fill();
+     ctx.closePath();
+
+     // Lineas del sol
+     const numLines = 17;
+     const angleStep = (2 * Math.PI) / numLines;
+     const lineLength = unit * 11;
+     for (let i = 0; i < numLines; i++) {
+          const angle = i * angleStep;
+          const startX = unit * 40 + Math.cos(angle) * unit * 6;
+          const startY = unit * 0 + Math.sin(angle) * unit * 6;
+          const endX = unit * 40 + Math.cos(angle) * lineLength;
+          const endY = unit * 0 + Math.sin(angle) * lineLength;
+          ctx.beginPath();
+          ctx.moveTo(startX, startY);
+          ctx.lineTo(endX, endY);
+          ctx.stroke();
+          ctx.closePath();
+     }
+}
+
+
+
+
